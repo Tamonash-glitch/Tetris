ba39db7d6990113cb4dd0dbbeae1cf1feb213d00
Tamonash <tamonash004@gmail.com>
2023-09-16 20:24:40 +0530

    created most of the skeleton layout for tetris game

---
+++ Main.py
@@ -0,0 +1,31 @@
+import pygame,sys
+pygame.init()
+from game import Game
+
+width=300
+height=600
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption("Tetris")
+
+clock=pygame.time.Clock()
+
+game=Game()
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                game.move_left()
+            if event.key==pygame.K_RIGHT:
+                game.move_right()
+            if event.key==pygame.K_DOWN:
+                game.move_down()
+    screen.fill((44,44,127))
+    game.draw(screen=screen)
+
+
+    pygame.display.update()
+    clock.tick(60)
---
+++ block.py
@@ -0,0 +1,28 @@
+from colors import Colors
+from position import Position
+import pygame
+class Block:
+    def __init__(self,id):
+        self.id=id
+        self.cells={}
+        self.cell_size=30
+        self.row_offset=0
+        self.column_offset=0
+        self.rotation_state=0
+        self.colors=Colors.get_cell_colors()
+    def move(self,rows,columns):
+        self.row_offset+=rows
+        self.column_offset+=columns
+    def get_cell_positions(self):
+        tiles=self.cells[self.rotation_state]
+        moved_tiles=[]
+        for position in tiles:
+            position=Position(position.row+self.row_offset,position.column+self.column_offset)
+            moved_tiles.append(position)
+        return moved_tiles
+    
+    def draw(self,screen):
+        tiles=self.get_cell_positions()
+        for tile in tiles:
+            tile_rect=pygame.Rect(tile.column*self.cell_size+1,tile.row*self.cell_size+1,self.cell_size-1,self.cell_size-1)
+            pygame.draw.rect(screen,self.colors[self.id],tile_rect)
